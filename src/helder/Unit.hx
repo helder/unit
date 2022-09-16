0e5456f1b693db2a248d17c5504c4ddfaddeb0fa
Ben Merckx <ben@codeurs.be>
2022-09-16 10:19:00 +0200

    Export a single suite function for now. Overloading makes the compiler hide important error messages.

--- src/helder/Unit.hx
+++ src/helder/Unit.hx
@@ -2,17 +2,17 @@ package helder;
 
 import helder.unit.Suite;
 
-overload extern inline function suite<S, T>(?options: SuiteOptions<S, T>, defineTests: (test: TestMethod<S, T>) -> Void) {
+function suite<S, T>(?options: SuiteOptions<S, T>, defineTests: (test: TestMethod<S, T>) -> Void) {
   final suite = new Suite<S, T>(options);
   defineTests(suite.test);
   return suite;
 }
 
-overload extern inline function suite<S, T>(options: SuiteOptions<S, T>) {
+/*inline function suite<S, T>(options: SuiteOptions<S, T>) {
   return (defineTests: (test: TestMethod<S, T>) -> Void) -> {
     return suite(options, defineTests);
   }
-}
+}*/
 
 final assert = helder.unit.Assert.assert;
 
